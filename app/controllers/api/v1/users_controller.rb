d5449d53897613677ddc226e2aa5e519c7fb0b2a
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-01-03 18:21:43 +0000

    hotfix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::UsersController < Api::V1::BaseController
     def show
         user = User.find(params[:id])
-        hash = Hash.new
-        hash[user][:operations] = user.operation.all
-        render(json: hash)
+        
+        render(json: user)
     end
 end
