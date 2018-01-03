68468eeaba5224231399b6a143cd0431a9e10edb
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-01-03 16:51:26 +0000

    user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < Api::V1::BaseController
     def show
         user = User.find(params[:id])
     
-        render(json: Api::V1::UserSerializer.new(user).to_json)
+        render(json: user)
     end
 end
