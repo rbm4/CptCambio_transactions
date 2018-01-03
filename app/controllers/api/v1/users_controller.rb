45b13de9c108b864cf24ed898c6031ca9390b8ca
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-01-03 16:56:33 +0000

    user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     def show
         user = User.find(params[:id])
         hash = Hash.new
-        hash[user][:operations] = user.operations.all
+        hash[user][:operations] = user.operation.all
         render(json: hash)
     end
 end
