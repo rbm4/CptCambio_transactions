24438f060f0afb1ff607f89d338a294568a1822b
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-01-04 21:05:36 +0000

    logger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
     decipher.iv = ENV["CIPHER_IV"]
     
     plain = decipher.update(params[:message]) + decipher.final
+    p plain
     @params = eval(plain)
     if @params['key'] == ENV["TRANSACTION_KEY"]
       true
