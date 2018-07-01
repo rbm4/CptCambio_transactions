9f76ed663f58a812f2c620554d70824623ed1da5
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-07-01 21:58:59 +0000

    Commit Scriptado

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,8 +2,7 @@ class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request, raise: false
     skip_before_action :verify_authenticity_token
     def authenticate 
-        command = AuthenticateUser.new
-        command.initialize(params[:email],params[:password])
+        command = AuthenticateUser.new(params[:email],params[:password])
         command = AuthenticateUser.call(params[:email], params[:password]) 
         
         if command.success? 
