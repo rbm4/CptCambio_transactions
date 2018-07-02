376467ebd86f1a996f87adf93254befeec003bdc
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-07-02 01:42:01 +0000

    Commit Scriptado

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,12 +2,12 @@ class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request, raise: false
     skip_before_action :verify_authenticity_token
     def authenticate 
-        command = AuthenticateUser.new(params[:email],params[:password])
         command = AuthenticateUser.call(params[:email], params[:password]) 
         
         if command.success? 
             render json: { auth_token: command.result } 
         else 
+            p JSON.parse({ error: command.errors })
             render json: { error: command.errors }, status: :unauthorized 
         end 
     end
